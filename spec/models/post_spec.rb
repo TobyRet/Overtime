492ae58ba91b41cbcb6866dfab1309dc753672aa
Toby Retallick <toby.retallick@gmail.com>
2017-10-27 23:14:32 +0100

    Implement user association with posts with tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,8 @@ describe Post, type: :model do
   describe 'Creation' do
 
     before do
-      @post = Post.create(date: Date.today, rationale: 'Anything')
+      @user = User.create!(first_name: 'T', last_name: 'R', email: 't@t.com', password: 'asdfasdf', password_confirmation: 'asdfasdf')
+      @post = Post.create(date: Date.today, user_id: @user.id, rationale: 'Anything')
     end
     
     it 'can be created' do
