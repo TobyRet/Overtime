c2ed8ec5c2cd62d5229746009ed190b7be3b2b24
Toby Retallick <toby.retallick@gmail.com>
2017-10-28 22:51:06 +0100

    Add twitter bootstrap

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,11 +7,10 @@ describe Post, type: :model do
       @user = User.create!(first_name: 'T', last_name: 'R', email: 't@t.com', password: 'asdfasdf', password_confirmation: 'asdfasdf')
       @post = Post.create(date: Date.today, user_id: @user.id, rationale: 'Anything')
     end
-    
+
     it 'can be created' do
       expect(@post).to be_valid
     end
-
 
     it 'cannot be created without date and rationale' do
       @post.date = nil
