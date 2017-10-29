359918380a770745028be470d701ebb9d4a4bdd5
Toby Retallick <toby.retallick@gmail.com>
2017-10-29 23:37:26 +0000

    Refactor tests with FactoryGirl

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :development, :test do
   gem 'selenium-webdriver'
   gem 'database_cleaner'
   gem 'rspec-rails'
+  gem 'factory_girl_rails', '~> 4.9'
 end
 
 group :development do
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,8 +4,8 @@ describe Post, type: :model do
   describe 'Creation' do
 
     before do
-      @user = User.create!(first_name: 'T', last_name: 'R', email: 't@t.com', password: 'asdfasdf', password_confirmation: 'asdfasdf')
-      @post = Post.create(date: Date.today, user_id: @user.id, rationale: 'Anything')
+      @user = FactoryGirl.create(:user)
+      @post = FactoryGirl.create(:post) 
     end
 
     it 'can be created' do
