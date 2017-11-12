02030c0d02b922e15cc310b1f75f61684fedaeba
Toby Retallick <toby.retallick@gmail.com>
2017-11-12 20:11:53 +0000

    Only Admin Users can edit posts

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -9,14 +9,23 @@ describe 'Navigate' do
   describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
+      visit edit_post_path(@post)
     end
 
     it 'has a status that can be edited on the form' do
-      visit edit_post_path(@post)
-
       choose('post_status_approved')
 
       expect(@post.status).to eq('approved')
+    end
+
+    it 'cannot be added by a non admin' do
+      logout :user
+      @user = FactoryGirl.create(:user)
+      login_as(@user, :scope => :user)
+
+      visit edit_post_path(@post)
+
+      expect(page).to_not have_content('Approved')
     end
   end
 end
