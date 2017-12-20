f5eb14552a493ebfec271ac0941732fd07c8c53e
Toby Retallick <toby.retallick@gmail.com>
2017-12-20 23:21:53 +0000

    Created resource for Audit Log

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :audit_logs
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name, :phone_number
 
