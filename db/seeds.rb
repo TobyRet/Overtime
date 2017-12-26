ab7f80cccfc9c58bbdfc601688794c4417f2af11
Toby Retallick <toby.retallick@gmail.com>
2017-12-26 21:21:03 +0000

    Updated audit log factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-@user= User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Toby', last_name: 'Retallick')
+@user= User.create!(email: 'test@test.com', phone_number: '0876789879', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Toby', last_name: 'Retallick')
 
 puts 'User has been created'
 
-AdminUser.create(email: 'admin@admin.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Admin', last_name: 'User')
+AdminUser.create!(email: 'admin@admin.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Admin', last_name: 'User')
 
 puts 'Admin user has been created'
 
@@ -11,3 +11,9 @@ puts 'Admin user has been created'
 end
 
 puts '100 posts have been created'
+
+100.times do |audit_log|
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts '100 audit logs have been created'
